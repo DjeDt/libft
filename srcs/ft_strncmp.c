4def963ca90f6f0f32f67e4d2ff13d89e5f4e0fa
Dje <dje@debian>
2017-04-17 22:06:36 +0200

    strncmp

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -14,18 +14,16 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*str1;
-	unsigned char	*str2;
+	size_t i;
 
 	i = 0;
-	str1 = (unsigned char*)s1;
-	str2 = (unsigned char*)s2;
-	while ((i < n) && (str1[i] || str2[i]))
+	if (n == 0)
+		return (0);
+	while ((s1[i] || s2[i]) && (i + 1) < n)
 	{
-		if (str1[i] != str2[i])
-			return (str1[i] - str2[i]);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
