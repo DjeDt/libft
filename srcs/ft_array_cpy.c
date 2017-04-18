95a684764fbd41222c28b6b3236b11d6622cd322
Djelali DINAUT <ddinaut@e1r5p14.42.fr>
2017-04-18 21:31:07 +0200

    array modified

--- srcs/ft_array_cpy.c
+++ srcs/ft_array_cpy.c
@@ -6,54 +6,25 @@
 /*   By: ddinaut <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 12:19:02 by ddinaut           #+#    #+#             */
-/*   Updated: 2017/03/23 18:47:11 by ddinaut          ###   ########.fr       */
+/*   Updated: 2017/04/18 17:05:20 by ddinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	nb_tab(char **tab)
-{
-	int	count;
-
-	count = 0;
-	while (tab[count])
-		count++;
-	return (count);
-}
-
-static char	**ft_ret(void)
-{
-	char **ret;
-
-	if (!(ret = (char**)malloc(sizeof(char*) * 1)))
-		return (NULL);
-	ret[0] = NULL;
-	return (ret);
-}
-
 char		**ft_array_cpy(char **tab)
 {
-	int		len;
 	int		count;
-	int		count2;
 	char	**ret;
 
-	len = 0;
-	count2 = -1;
-	if ((*tab) == NULL)
-		return (ft_ret());
-	count = nb_tab(tab);
-	if (!(ret = (char**)malloc(sizeof(char*) * (count + 1))))
+	count = 0;
+	if (!(ret = (char**)malloc(sizeof(char*) * ft_array_len(tab) + 1)))
 		return (NULL);
-	while (++count2 < count)
+	while (tab[count] != NULL)
 	{
-		len = ft_strlen(tab[count2]);
-		if (!(ret[count2] = (char*)malloc(sizeof(char) * len + 1)))
-			return (NULL);
-		ft_strcpy(ret[count2], tab[count2]);
-		ret[count2][len + 1] = '\0';
+		ret[count] = ft_strdup(tab[count]);
+		count++;
 	}
-	ret[count2] = 0;
+	tab[count] = NULL;
 	return (ret);
 }
--- srcs/ft_array_free.c
+++ srcs/ft_array_free.c
@@ -6,7 +6,7 @@
 /*   By: ddinaut <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 19:24:33 by ddinaut           #+#    #+#             */
-/*   Updated: 2017/03/24 12:28:12 by ddinaut          ###   ########.fr       */
+/*   Updated: 2017/04/18 20:17:04 by ddinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@
 void	ft_array_free(char ***tab)
 {
 	size_t	count;
+	size_t	count2;
 	char	**tmp;
 
-	count = -1;
+	count = 0;
+	count2 = 0;
 	tmp = (*tab);
+	while (tmp[count2] != NULL)
+		count2++;
 	if (tab != NULL)
 	{
-		while (tmp[++count])
+		while (count < count2)
+		{
 			free(tmp[count]);
+			count++;
+		}
 		free(tmp);
-		tmp = NULL;
 	}
 }
