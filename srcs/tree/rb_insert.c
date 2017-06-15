38b77ebf6ba55bda092df2fbea5a41a54b388b41
Djelali DINAUT <ddinaut@e3r10p4.42.fr>
2017-06-15 18:55:43 +0200

    sddf

--- srcs/tree/rb_insert.c
+++ srcs/tree/rb_insert.c
@@ -6,7 +6,7 @@
 /*   By: ddinaut <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/12 18:44:01 by ddinaut           #+#    #+#             */
-/*   Updated: 2017/06/15 18:19:59 by ddinaut          ###   ########.fr       */
+/*   Updated: 2017/06/15 18:50:13 by ddinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,60 @@ t_rb_node	*rb_create(void *data)
 	return (new);
 }
 
+void rb_insert(rbtree t, void* key, compare_func compare)
+{
+    node inserted_node;
+
+	inserted_node = new_node(key, RED, NULL, NULL);
+    if (t->root == NULL)
+        t->root = inserted_node;
+	else
+	{
+        node n = t->root;
+        while (1)
+		{
+            int comp_result = compare(key, n->key);
+            if (comp_result == 0)
+			{
+                free (inserted_node);
+                return;
+            }
+			else if (comp_result < 0)
+			{
+                if (n->left == NULL)
+				{
+                    n->left = inserted_node;
+                    break;
+                }
+				else
+                    n = n->left;
+            }
+			else
+			{
+                assert (comp_result > 0);
+                if (n->right == NULL)
+				{
+                    n->right = inserted_node;
+                    break;
+                }
+				else
+                    n = n->right;
+            }
+        }
+        inserted_node->parent = n;
+    }
+    insert_case1(t, inserted_node);
+    verify_properties(t);
+}
+
+void	rb_insert(struct s_rb_node **root, void *data, int (*f)(void *, void *))
+{
+	t_rb_node *insert;
+
+
+}
+
+/*
 void	rb_insert(struct s_rb_node **root, void *data, int (*f)(void *, void *))
 {
 	if (data == NULL)
@@ -88,3 +142,4 @@ void	rb_insert(struct s_rb_node **root, void *data, int (*f)(void *, void *))
 	}
 	insert_case1((*root));
 }
+*/
